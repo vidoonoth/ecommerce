ade72ec613c319f0ff746dc0c92991c30e088bce
vidoonoth <alfinvidoo@gmail.com>
2025-10-24 10:36:24 +0700

    `Updated nav-link component and products view with CSS class changes`

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     {{-- products --}}
 
-    <div class="bg-[#fffdfc] p-4 w-fit my-2 mx-2 rounded-lg">
+    <div class="w-fit mx-8 my-2 border-b-2">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Produk') }}
         </h2>
